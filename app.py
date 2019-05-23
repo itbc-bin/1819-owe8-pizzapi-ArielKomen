88a58846b78e5a472c9abc1e09bf8105ffe4f00d
ArielKomen <ariel.komen@filternet.nl>
2019-05-23 11:18:19 +0200

    API voorbeeld om misschien ooit iets mee te doen

--- app.py
+++ app.py
@@ -3,10 +3,32 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 pizzaDB = [
-            {'name': 'tonno'},
-            {'name': 'salami'},
-            {'name': 'hawaii'}
+            {'name': 'tonno', 'prijs': 10},
+            {'name': 'salami', 'prijs': 5},
+            {'name': 'hawaii', 'prijs': 8}
           ]
+@app.route("/<string:name>", methods=['GET'])
+def getOnePizza(name):
+    resultPizza = []
+    for pizza in pizzaDB:
+        if pizza['name'] == name:
+            resultPizza.append(pizza)
+    return jsonify({'pizzaDB':resultPizza})
+
+@app.route("/", methods=['POST'])
+def addOnePizza():
+    pizza = {'name' : request.json['name'], 'prijs' : request.json['prijs']}
+    pizzaDB.append(pizza)
+    return jsonify({'pizzaDB' : pizzaDB})
+
+@app.route("/<string:name>", methods=['DELETE'])
+def delPizza(name):
+    resultPizza = []
+    for pizza in pizzaDB:
+        if pizza['name'] == name:
+            resultPizza.append(pizza)
+    pizzaDB.remove(resultPizza[0])
+    return jsonify({'pizzaDB': pizzaDB})
 
 @app.route("/", methods=['GET'])
 def getPizza():
